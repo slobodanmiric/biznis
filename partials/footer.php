ad6f5ccce36ad590870eb5cb31da0176b5bd0efb
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-11 21:57:38 +0100

    partials

---
+++ partials/footer.php
@@ -0,0 +1,25 @@
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <div class="col">
+                <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-envelope"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-phone"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
