3f1a7e00c9587553cce32fd08e5740acff54eeb2
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-11 21:44:05 +0100

    footer html

--- index.php
+++ index.php
@@ -21,5 +21,29 @@
             <li><a href=""><i class="fas fa-search"></i></a></li>
         </ul>
     </nav>
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <div class="col">
+                <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-envelope"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-phone"></i>
+                <p>Main streer 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
