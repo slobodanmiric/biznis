d4d7105ab0d09a0ba0a8bc8a889d92d7a8de63ad
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-12 20:19:20 +0100

    header html

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php include "partials/header.php" ?>
 
-    <h1>Ovde cemo sutra kontent</h1>
+    <!-- header -->
+    <header>
+        <h1>Trusted and <br> professionals advisers</h1>
+        <h3>For your business</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+        <button>About us</button>
+    </header>
 
 <?php include "partials/footer.php" ?>
 
