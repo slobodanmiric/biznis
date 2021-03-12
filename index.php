4151846bfb66fcff96980ecc288158c3c751d521
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-12 21:57:58 +0100

    business

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "partials/header.php" ?>
 
     <!-- header -->
-    <header>
+    <header class="mainHeader">
         <h1>Trusted and <br> professionals advisers</h1>
         <h3>For your business</h3>
         <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
@@ -82,6 +82,38 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
         </article>
     </section>
+
+     <!-- headings -->
+     <section class="business">
+        <header class="container headings black">
+            <article>
+                <h2>03</h2>
+            </article>
+            <article>
+                <h3>Services</h3>
+                <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+                <h4>Get your Business in 4 easy steps</h4>
+                <button>About us</button>
+            </article>
+        </header>
+        <div class="counter container">
+            <div class="col">
+                <h2>18</h2>
+                <h4>Nominations</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>5</h2>
+                <h4>Awwards</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>45</h2>
+                <h4>Agencies</h4>
+                <hr>
+            </div>
+        </div>
+     </section>
 
 <?php include "partials/footer.php" ?>
 
